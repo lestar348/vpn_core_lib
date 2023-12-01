e12285486f955a2969e47b494f9df3854fa4f125
lestar348 <lestar348@gmail.com>
2023-12-01 14:17:12 +0300

    linter fix

--- src/main/java/com/synaptic/vpn_core_lib/interfaces/LogSettings.kt
+++ src/main/java/com/synaptic/vpn_core_lib/interfaces/LogSettings.kt
@@ -1,8 +1,7 @@
 package com.synaptic.vpn_core_lib.interfaces
 
 import com.synaptic.vpn_core_lib.models.LogLevelModel
-import com.synaptic.vpn_core_lib.setup.MmkvManager
-import com.tencent.mmkv.MMKV
+
 
 interface ILogSettings {
 
